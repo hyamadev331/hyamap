3108e4c686afb8ba8e5f10a4c3b05604f120876a
hyamadev331 <hyamadev331@gmail.com>
2024-01-23 22:02:02 +0900

    feat: Add infowindow and center location information

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -2,35 +2,36 @@ import axios from 'axios';
 import Backdrop from '@mui/material/Backdrop';
 import Button from '@mui/material/Button';
 import CircularProgress from '@mui/material/CircularProgress';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 import Grid from '@mui/material/Grid';
-import { React, useState } from 'react';
+import { React, useState, useRef, useCallback } from 'react';
 
 const containerStyle = {
   width: '100%',
   height: '95vh'
 };
 
-//初期位置
-const center = {
-  lat: 31.9076,
-  lng: 131.4202
-};
-
 const App = () => {
+  const [center, setCenter] = useState({ lat: 31.9076, lng: 131.4202 }); // 中心位置 
   const [locationData, setLocationData] = useState([]); // 周辺情報
   const [isLoading, setIsLoading] = useState(false);// ローディング中かどうか
+  const mapRef = useRef();
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map;
+  }, []);
 
   const params = {
-    "location": "31.9076 131.4202",
-    "radius": 10000,
-    "keywords": "restaurant,cafe,food",
+    "location": `${center.lat} ${center.lng}`,
+    "radius": 3000,// 半径3km
+    "keywords": "レストラン",
   };
 
   // 位置、半径、キーワードから周辺情報を検索する 
   const getLocationInfo = async (params) => {
+    // 検索前に位置情報を初期化する
+    setLocationData([]);
     setIsLoading(true);
-    axios.post("http://localhost:8000/search_nearby_places", params, {
+    axios.post(`http://${process.env.REACT_APP_BACKEND_IP}:8000/search_nearby_places`, params, {
       headers: {
         'Content-Type': 'application/json'
       }
@@ -43,6 +44,8 @@ const App = () => {
     });
   }
 
+  const [selectedLocation, setSelectedLocation] = useState(null);
+
   return (
     <div className="App">
       <LoadScript
@@ -52,10 +55,31 @@ const App = () => {
           mapContainerStyle={containerStyle}
           center={center}
           zoom={15}
+          onLoad={onMapLoad}
+          onDragEnd={() => {
+            const newCenter = mapRef.current.getCenter();
+            setCenter({ lat: newCenter.lat(), lng: newCenter.lng() });
+            console.log(center);
+          }}
         >
           {locationData.map((location, index) => (
-            <Marker key={index} position={location[0]} />
+            <Marker key={index} position={location[0]} onClick={() => {
+              setSelectedLocation(location);
+            }} />
           ))}
+
+          {selectedLocation && (
+            <InfoWindow
+              position={selectedLocation[0]}
+              onCloseClick={() => {
+                setSelectedLocation(null);
+              }}
+            >
+              <div>
+                <p>{selectedLocation[1]}</p>
+              </div>
+            </InfoWindow>
+          )}
         </GoogleMap>
       </LoadScript>
       <Grid container spacing={2} sx={{ mt: 1 }}>
